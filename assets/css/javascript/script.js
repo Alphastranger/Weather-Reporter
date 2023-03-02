1a3db38ec935e45e3f426895ae7cdcfd5a12ba85
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-03-02 13:00:23 -0500

    Some preliminary javascript

--- assets/css/javascript/script.js
+++ assets/css/javascript/script.js
@@ -1,3 +1,4 @@
+var fiveDay = document.getElementById('fiveday')
 let apiKey = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
 fetch(apiKey)
     .then(function(response){
@@ -5,4 +6,10 @@ fetch(apiKey)
     })
     .then(function (data){
         console.log(data)
+        for (var i=0; i < data.length; i++) {
+            var createDisplayEl = document.createElement('p')
+            createDisplayEl.textContent = data[i].description;
+            fiveDay.appendChild(createDisplayEl)
+
+        }
     })
