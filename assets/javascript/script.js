f2cb2d04a2d7894210bb6bc2ed3418eeaf2ac25e
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-03-04 10:23:38 -0500

    Fixed the 401 error

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 var fiveDay = document.getElementById('fiveday')
-let apiKey = 'https://api.openweathermap.org/data/2.5/forecast?lat={38.2527}&lon={85.7585}&appid={a0dfdb5ddf4248081a0e3da0b4bb8938}'
+let apiKey = 'https://api.openweathermap.org/data/2.5/forecast?lat=38.2527&lon=85.7585&appid=a0dfdb5ddf4248081a0e3da0b4bb8938'
 fetch(apiKey)
     .then(function(response){
         return response.json ();
@@ -8,7 +8,7 @@ fetch(apiKey)
         console.log(data)
         for (var i=0; i < data.length; i++) {
             var createDisplayEl = document.createElement('p')
-            createDisplayEl.textContent = data[i].description;
+            createDisplayEl.textContent = data[i].temp;
             fiveDay.appendChild(createDisplayEl)
 
         }
