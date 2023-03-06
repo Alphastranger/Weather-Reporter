88a909dd495696a042dff94995642e9e74aaf148
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-03-05 21:07:49 -0500

    Created if else statements for icons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -65,7 +65,7 @@ fetch(apiKey)
             cityName.appendChild(weatherIcon)
             weatherIcon.classList.add('fa-sharp')
             weatherIcon.classList.add('fa-solid')
-            weatherIcon.classList.add('fa-cloud')
+            weatherIcon.classList.add('fa-umbrella')
         } else if (data.list[0].weather[0].main === 'Clear'){
             var weatherIcon = document.createElement('i')
             cityName.appendChild(weatherIcon)
@@ -83,7 +83,7 @@ fetch(apiKey)
             cityName.appendChild(weatherIcon)
             weatherIcon.classList.add('fa-sharp')
             weatherIcon.classList.add('fa-solid')
-            weatherIcon.classList.add('fa-block-quest')
+            weatherIcon.classList.add('fa-block-question')
         }
 
 
@@ -91,26 +91,181 @@ fetch(apiKey)
         let tomWeath = document.createElement('p')
         tomorrow.appendChild(tomWeath)
         tomWeath.textContent = 'Temp: ' + data.list[1].main.temp + 'K degrees\n' + "Wind: " + data.list[1].wind.speed +'\n' + 'Humidity: ' + data.list[1].main.humidity
+        if (data.list[1].weather[0].main === 'Clouds'){
+            var weatherIconTom = document.createElement('i')
+            tomorrow.appendChild(weatherIcon)
+            weatherIconTom.classList.add('fa-sharp')
+            weatherIconTom.classList.add('fa-solid')
+            weatherIconTom.classList.add('fa-cloud')
+        } else if (data.list[1].weather[0].main === 'Rain' || data.list[1].weather[0].main === 'Drizzle' || data.list[1].weather[0].main === 'Thunderstorm'){
+            var weatherIconTom = document.createElement('i')
+            tomorrow.appendChild(weatherIcon)
+            weatherIconTom.classList.add('fa-sharp')
+            weatherIconTom.classList.add('fa-solid')
+            weatherIconTom.classList.add('fa-umbrella')
+        } else if (data.list[1].weather[0].main === 'Clear'){
+            var weatherIconTom = document.createElement('i')
+            tomorrow.appendChild(weatherIcon)
+            weatherIconTom.classList.add('fa-sharp')
+            weatherIconTom.classList.add('fa-solid')
+            weatherIconTom.classList.add('fa-sun')
+        } else if (data.list[1].weather[0].main === 'Snow') {
+            var weatherIconTom = document.createElement('i')
+            tomorrow.appendChild(weatherIcon)
+            weatherIconTom.classList.add('fa-sharp')
+            weatherIconTom.classList.add('fa-solid')
+            weatherIconTom.classList.add('fa-snowflake')
+        } else {
+            var weatherIconTom = document.createElement('i')
+            tomorrow.appendChild(weatherIcon)
+            weatherIconTom.classList.add('fa-sharp')
+            weatherIconTom.classList.add('fa-solid')
+            weatherIconTom.classList.add('fa-block-question')
+        }
 
         //Day three weather
         let daythreeWeath = document.createElement('p')
         daythree.appendChild(daythreeWeath)
         daythreeWeath.textContent = 'Temp: ' + data.list[2].main.temp + 'K degrees\n' + "Wind: " + data.list[2].wind.speed +'\n' + 'Humidity: ' + data.list[2].main.humidity
+        if (data.list[2].weather[0].main === 'Clouds'){
+            var weatherIconThr = document.createElement('i')
+            daythree.appendChild(weatherIcon)
+            weatherIconThr.classList.add('fa-sharp')
+            weatherIconThr.classList.add('fa-solid')
+            weatherIconThr.classList.add('fa-cloud')
+        } else if (data.list[2].weather[0].main === 'Rain' || data.list[2].weather[0].main === 'Drizzle' || data.list[2].weather[0].main === 'Thunderstorm'){
+            var weatherIconThr = document.createElement('i')
+            daythree.appendChild(weatherIcon)
+            weatherIconThr.classList.add('fa-sharp')
+            weatherIconThr.classList.add('fa-solid')
+            weatherIconThr.classList.add('fa-umbrella')
+        } else if (data.list[2].weather[0].main === 'Clear'){
+            var weatherIconThr = document.createElement('i')
+            daythree.appendChild(weatherIcon)
+            weatherIconThr.classList.add('fa-sharp')
+            weatherIconThr.classList.add('fa-solid')
+            weatherIconThr.classList.add('fa-sun')
+        } else if (data.list[2].weather[0].main === 'Snow') {
+            var weatherIconThr = document.createElement('i')
+            daythree.appendChild(weatherIcon)
+            weatherIconThr.classList.add('fa-sharp')
+            weatherIconThr.classList.add('fa-solid')
+            weatherIconThr.classList.add('fa-snowflake')
+        } else {
+            var weatherIconThr = document.createElement('i')
+            daythree.appendChild(weatherIcon)
+            weatherIconThr.classList.add('fa-sharp')
+            weatherIconThr.classList.add('fa-solid')
+            weatherIconThr.classList.add('fa-block-question')
+        }
 
         //Day four weather
         let dayfourWeath = document.createElement('p')
         dayfour.appendChild(dayfourWeath)
         dayfourWeath.textContent = 'Temp: ' + data.list[3].main.temp + 'K degrees\n' + "Wind: " + data.list[3].wind.speed +'\n' + 'Humidity: ' + data.list[3].main.humidity
+        if (data.list[3].weather[0].main === 'Clouds'){
+            var weatherIconFou = document.createElement('i')
+            dayfour.appendChild(weatherIcon)
+            weatherIconFou.classList.add('fa-sharp')
+            weatherIconFou.classList.add('fa-solid')
+            weatherIconFou.classList.add('fa-cloud')
+        } else if (data.list[3].weather[0].main === 'Rain' || data.list[3].weather[0].main === 'Drizzle' || data.list[3].weather[0].main === 'Thunderstorm'){
+            var weatherIconFou = document.createElement('i')
+            dayfour.appendChild(weatherIcon)
+            weatherIconFou.classList.add('fa-sharp')
+            weatherIconFou.classList.add('fa-solid')
+            weatherIconFou.classList.add('fa-umbrella')
+        } else if (data.list[3].weather[0].main === 'Clear'){
+            var weatherIconFou = document.createElement('i')
+            dayfour.appendChild(weatherIcon)
+            weatherIconFou.classList.add('fa-sharp')
+            weatherIconFou.classList.add('fa-solid')
+            weatherIconFou.classList.add('fa-sun')
+        } else if (data.list[3].weather[0].main === 'Snow') {
+            var weatherIconFou = document.createElement('i')
+            dayfour.appendChild(weatherIcon)
+            weatherIconFou.classList.add('fa-sharp')
+            weatherIconFou.classList.add('fa-solid')
+            weatherIconFou.classList.add('fa-snowflake')
+        } else {
+            var weatherIconFou = document.createElement('i')
+            dayfour.appendChild(weatherIcon)
+            weatherIconFou.classList.add('fa-sharp')
+            weatherIconFou.classList.add('fa-solid')
+            weatherIconFou.classList.add('fa-block-question')
+        }
 
         //Day five weather
         let dayfiveWeath = document.createElement('p')
         dayfive.appendChild(dayfiveWeath)
         dayfiveWeath.textContent = 'Temp: ' + data.list[4].main.temp + 'K degrees\n' + "Wind: " + data.list[4].wind.speed +'\n' + 'Humidity: ' + data.list[4].main.humidity
+        if (data.list[4].weather[0].main === 'Clouds'){
+            var weatherIconFiv = document.createElement('i')
+            dayfive.appendChild(weatherIcon)
+            weatherIconFiv.classList.add('fa-sharp')
+            weatherIconFiv.classList.add('fa-solid')
+            weatherIconFiv.classList.add('fa-cloud')
+        } else if (data.list[4].weather[0].main === 'Rain' || data.list[4].weather[0].main === 'Drizzle' || data.list[4].weather[0].main === 'Thunderstorm'){
+            var weatherIconFiv = document.createElement('i')
+            dayfive.appendChild(weatherIcon)
+            weatherIconFiv.classList.add('fa-sharp')
+            weatherIconFiv.classList.add('fa-solid')
+            weatherIconFiv.classList.add('fa-umbrella')
+        } else if (data.list[4].weather[0].main === 'Clear'){
+            var weatherIconFiv = document.createElement('i')
+            dayfive.appendChild(weatherIcon)
+            weatherIconFiv.classList.add('fa-sharp')
+            weatherIconFiv.classList.add('fa-solid')
+            weatherIconFiv.classList.add('fa-sun')
+        } else if (data.list[4].weather[0].main === 'Snow') {
+            var weatherIconFiv = document.createElement('i')
+            dayfive.appendChild(weatherIcon)
+            weatherIconFiv.classList.add('fa-sharp')
+            weatherIconFiv.classList.add('fa-solid')
+            weatherIconFiv.classList.add('fa-snowflake')
+        } else {
+            var weatherIconFiv = document.createElement('i')
+            dayfive.appendChild(weatherIcon)
+            weatherIconFiv.classList.add('fa-sharp')
+            weatherIconFiv.classList.add('fa-solid')
+            weatherIconFiv.classList.add('fa-block-question')
+        }
 
         //Day six weather
         let daysixWeath = document.createElement('p')
         daysix.appendChild(daysixWeath)
         daysixWeath.textContent = 'Temp: ' + data.list[5].main.temp + 'K degrees\n' + "Wind: " + data.list[5].wind.speed +'\n' + 'Humidity: ' + data.list[5].main.humidity
+        if (data.list[5].weather[0].main === 'Clouds'){
+            var weatherIconSix = document.createElement('i')
+            daysix.appendChild(weatherIcon)
+            weatherIconSix.classList.add('fa-sharp')
+            weatherIconSix.classList.add('fa-solid')
+            weatherIconSix.classList.add('fa-cloud')
+        } else if (data.list[5].weather[0].main === 'Rain' || data.list[5].weather[0].main === 'Drizzle' || data.list[5].weather[0].main === 'Thunderstorm'){
+            var weatherIconSix = document.createElement('i')
+            daysix.appendChild(weatherIcon)
+            weatherIconSix.classList.add('fa-sharp')
+            weatherIconSix.classList.add('fa-solid')
+            weatherIconSix.classList.add('fa-umbrella')
+        } else if (data.list[5].weather[0].main === 'Clear'){
+            var weatherIconSix = document.createElement('i')
+            daysix.appendChild(weatherIcon)
+            weatherIconSix.classList.add('fa-sharp')
+            weatherIconSix.classList.add('fa-solid')
+            weatherIconSix.classList.add('fa-sun')
+        } else if (data.list[5].weather[0].main === 'Snow') {
+            var weatherIconSix = document.createElement('i')
+            daysix.appendChild(weatherIcon)
+            weatherIconSix.classList.add('fa-sharp')
+            weatherIconSix.classList.add('fa-solid')
+            weatherIconSix.classList.add('fa-snowflake')
+        } else {
+            var weatherIconSix = document.createElement('i')
+            daysix.appendChild(weatherIcon)
+            weatherIconSix.classList.add('fa-sharp')
+            weatherIconSix.classList.add('fa-solid')
+            weatherIconSix.classList.add('fa-block-question')
+        }
     
     })
     })
